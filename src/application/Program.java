2a26df3610b5bfb7038d73c6a96ddfed4c3b7869
tais <taisquaresma4@gmail.com>
2025-10-23 16:09:16 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,14 +25,22 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		
+		
 			System.out.println("\n=== TEST 3: seller findAll ===");
 		    list = sellerDao.findAll();
 			for(Seller ob : list) {
 				System.out.println(ob);
-		
-		
-
+	
 		}
+			
+			System.out.println("\n=== TEST 4: seller insert ===");
+			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department); 
+			sellerDao.insert(newSeller);
+			System.out.println("Insert! New id = " + newSeller.getId());
+			
+			
 	}
 }
 
