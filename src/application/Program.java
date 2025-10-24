c365325767f6c10d8c0e4c87b5a4b7d3a99a43a6
tais <taisquaresma4@gmail.com>
2025-10-24 16:39:58 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,11 @@ public class Program {
 			System.out.println("Insert! New id = " + newSeller.getId());
 			
 			
+			System.out.println("\n=== TEST 5: seller update ===");
+			seller = sellerDao.findById(1);
+			seller.setName("Martha Waine");
+			sellerDao.update(seller);
+			System.out.println("Update completed");
 	}
 }
 
